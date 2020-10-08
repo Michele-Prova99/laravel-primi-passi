96cfe404851a28754b7fe75be74218bdae75206a
Michele Prova <michele.prova99@gmail.com>
2020-10-08 16:50:03 +0200

    inizio

---
+++ resources/views/prodotti.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>La Molisana</title>
+  </head>
+  <body>
+
+    <header>
+
+      <img src="https://www.lamolisana.it/wp-content/uploads/2020/04/marchio-sito-test.png" alt="">
+
+      <nav>
+        <ul>
+          <li><a href="{{ route('home') }}">Home</a></li>
+          <li><a href="{{ route('prodotti') }}">Prodotti</a></li>
+          <li><a href="{{ route('contatti') }}">Contatti</a></li>
+        </ul>
+      </nav>
+
+    </header>
+
+  </body>
+</html>
